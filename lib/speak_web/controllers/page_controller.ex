9e24d4c77e5a72d7181bf6ab4e9150cc5c4c7c39
Danilo Miranda <nilomiranda3@gmail.com>
2023-10-29 19:22:07 -0300

    chore: working on dashboard layout

--- lib/speak_web/controllers/page_controller.ex
+++ lib/speak_web/controllers/page_controller.ex
@@ -4,6 +4,9 @@ defmodule SpeakWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+
+    conn
+    |> put_layout(html: :dashboard)
+    |> render(:home)
   end
 end
