8dc11860ab893bf1a671b4fee46c1a666111b40a
Nilomiranda <nilomiranda3@gmail.com>
2023-10-29 21:50:54 -0300

    chore: apply dashboard layout to home

--- lib/speak_web/live/home_live.ex
+++ lib/speak_web/live/home_live.ex
@@ -6,4 +6,8 @@ defmodule SpeakWeb.HomeLive do
       <h1>This is the dashboard</h1>
     """
   end
+
+  def mount(_params, _session, socket) do
+    {:ok, socket, layout: {SpeakWeb.Layouts, :dashboard}}
+  end
 end
