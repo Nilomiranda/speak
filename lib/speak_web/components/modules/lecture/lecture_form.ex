48cdff6552bb5c1c3b4419f6f5997806ff80287b
Nilomiranda <nilomiranda3@gmail.com>
2023-11-01 11:59:07 -0300

    chore: extract lecture form to a component

---
+++ lib/speak_web/components/modules/lecture/lecture_form.ex
@@ -0,0 +1,31 @@
+defmodule LectureForm do
+  use SpeakWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+      <div>
+        <h2>New lecture</h2>
+
+        <.simple_form
+            for={@form}
+            id="create_lecture_form"
+            phx-submit="save"
+            phx-trigger-action={@trigger_submit}
+            action={~p"/lectures/new?_action=created&lecture_id=#{@lecture_id}"}
+            method="post"
+        >
+            <.error :if={@check_errors}>
+                Oops, something went wrong! Please check the errors below.
+            </.error>
+
+            <.input field={@form[:name]} label="Name" required />
+            <.input field={@form[:content]} label="Content" type="textarea" required />
+
+            <:actions>
+                <.button phx-disable-with="Saving..." class="w-full">Save</.button>
+            </:actions>
+        </.simple_form>
+      </div>
+    """
+  end
+end
