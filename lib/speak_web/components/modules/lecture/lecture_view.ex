afe7feea4dc1e70578c9cef6314d45868c012dc8
Nilomiranda <nilomiranda3@gmail.com>
2023-11-01 19:56:50 -0300

    chore: update lecture view UI

--- lib/speak_web/components/modules/lecture/lecture_view.ex
+++ lib/speak_web/components/modules/lecture/lecture_view.ex
@@ -4,8 +4,11 @@ defmodule LectureView do
   def render(assigns) do
     ~H"""
       <div>
-        <div class="flex items-center">
-          <h2><%= @lecture.name %></h2>
+        <div class="flex items-start">
+          <div class="flex flex-col">
+            <h2><%= @lecture.name %></h2>
+            <p><%= @lecture.description %></p>
+          </div>
 
           <.simple_form class="flex items-center !mt-0" method="delete" action={~p"/lectures/#{@lecture.id}"} for={%{:id => @lecture.id}}>
             <:actions>
@@ -14,9 +17,16 @@ defmodule LectureView do
           </.simple_form>
         </div>
 
-        <p><%= @lecture.description %></p>
+        <div class="flex items-stretch gap-x-2 mt-1">
+          <div class="border-2 border-green-900 flex-1 p-1">
+            <h3 class="text-brand text-xl">Content summary</h3>
+          </div>
 
-        <p><%= @lecture.content %></p>
+          <div class="border-2 border-pink-500 flex-1 p-1">
+            <p><%= @lecture.content %></p>
+          </div>
+
+        </div>
       </div>
     """
   end
