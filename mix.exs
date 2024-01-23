c44ca06c9c15471c9f2bfead81ee7eda3d196ae4
Danilo <nilomiranda3@gmail.com>
2024-01-22 19:41:57 -0800

    chore: update phoenix package version to `1.7.10`

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Speak.MixProject do
   defp deps do
     [
       {:pbkdf2_elixir, "~> 2.0"},
-      {:phoenix, "~> 1.7.2"},
+      {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
